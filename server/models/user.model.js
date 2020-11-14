5221c234dd9071e6c8d88637eb1adde781f7c13d
Patrick Hubbell <hubbell.64@osu.edu>
2020-11-14 16:31:00 -0500

    Update user schema for google auth purposes

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,10 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let userSchema = new Schema({
+  google_id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   email: {
     type: String,
     required: true,
     unique: true,
+  },
+  full_name: {
+    type: String,
+    trim: true,
   },
   hosted: [{ type: ObjectId, ref: "Session" }],
   participated: [{ type: ObjectId, ref: "Session" }],
