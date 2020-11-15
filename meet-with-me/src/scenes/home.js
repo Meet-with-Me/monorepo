ecb25ddfb3806e34c59c20c75b846eac869a5140
Patrick Hubbell <hubbell.64@osu.edu>
2020-11-15 14:08:23 -0500

    Create images folder in src to hold in-app images

--- meet-with-me/src/scenes/home.js
+++ meet-with-me/src/scenes/home.js
@@ -1,5 +1,7 @@
 import { useHistory } from 'react-router-dom';
 import { useProfileContext } from '../contexts/profile-context';
+import calendarImg from '../images/undraw_calendar_dutt.svg';
+import googleLogo from '../images/icons8-google.svg';
 
 export default function Home({ googleSignIn }) {
   const history = useHistory();
@@ -9,21 +11,14 @@ export default function Home({ googleSignIn }) {
     <div class="container" id="home-container">
       <div class="home-content">
         <div>
-          <img
-            className="d-none d-sm-block"
-            src={process.env.PUBLIC_URL + '/undraw_calendar_dutt.svg'}
-            width="200"
-          />
+          <img className="d-none d-sm-block" src={calendarImg} width="200" />
           <p>
             Want to plan an event but don't know everyone's availability? Send
             them a <span>Meet with Me</span>!
           </p>
         </div>
         <button class="btn" type="button" onClick={googleSignIn}>
-          <img
-            src={process.env.PUBLIC_URL + '/icons8-google.svg'}
-            width="40px"
-          />
+          <img src={googleLogo} width="40px" />
           Sign in with Google
         </button>
       </div>
