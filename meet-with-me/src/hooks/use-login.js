1625f15fba34aa64de6336a4a9b113d1aaf2d347
joseph-hughes <josephhughes129@gmail.com>
2020-11-14 19:49:32 -0500

    Add useLogin hook for logging in with Google

---
+++ meet-with-me/src/hooks/use-login.js
@@ -0,0 +1,28 @@
+import { useGoogleLogin } from 'react-google-login';
+import refreshGoogleToken from '../utils/refresh-google-token';
+
+const clientId = '877322371128-l9veif124rl10eirk6fc4godt9t9nssd.apps.googleusercontent.com';
+
+export default function useLogin(setIsSignedIn) {
+  const onSuccess = (response) => {
+    console.log('Login success... Response:', response);
+    refreshGoogleToken(response);
+    setIsSignedIn(true);
+  };
+
+  const onFailure = (response) => {
+    console.log('Login failed... Response:', response);
+  };
+
+  const { signIn: googleSignIn } = useGoogleLogin({
+    accessType: 'offline',
+    clientId,
+    isSignedIn: true,
+    onFailure,
+    onSuccess,
+  });
+
+  return {
+    googleSignIn,
+  };
+}
