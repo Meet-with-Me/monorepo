5bf9e0fc1beff2b17704b0ec8091e521f1469e7b
Patrick Hubbell <hubbell.64@osu.edu>
2020-11-22 13:16:26 -0500

    Show hamburger button instead of Sign Out button in header when screen is small

--- meet-with-me/src/components/header.js
+++ meet-with-me/src/components/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header({ googleSignOut, isSignedIn }) {
   return (
@@ -8,11 +10,35 @@ export default function Header({ googleSignOut, isSignedIn }) {
         <img src={process.env.PUBLIC_URL + '/m.jpg'} width="30" height="30" />
         Meet with Me
       </Link>
-      <div className="navbar-nav">
+      <div className="navbar-nav d-none d-sm-block" id="large-screen-options">
         {!!isSignedIn && (
           <button className="btn" type="button" onClick={googleSignOut}>
             Sign out
           </button>
+        )}
+      </div>
+      <div className="navbar-nav d-sm-none" id="small-screen-options">
+        {!!isSignedIn && (
+          <div className="dropdown">
+            <button
+              className="btn"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <FontAwesomeIcon className="icon" icon={faBars} />
+            </button>
+            <div
+              className="dropdown-menu dropdown-menu-right"
+              aria-labelledby="dropdownMenuButton"
+            >
+              <a href="#" className="dropdown-item" onClick={googleSignOut}>
+                Sign out
+              </a>
+            </div>
+          </div>
         )}
       </div>
     </nav>
